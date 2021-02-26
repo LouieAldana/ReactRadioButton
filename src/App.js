70a6eeea3c285515debd8b137ad25cdfb9df1e50
Louie Aldana <louiealdana@hotmail.com>
2021-02-26 15:52:11 -0800

    saving Radio Button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    value: "odin",
+    value2: "zeus"
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    const { value, value2 } = this.state;
+    return (
+      <form>
+        <h1>Current Value is : {value}</h1>
+        <h1>Current Value2 is : {value2}</h1>
+        <label>
+          odin
+          <input type="radio"
+            value="odin"
+            name="value"
+            checked={value === "odin"}
+            onChange={this.onChange} />
+        </label>
+        <label>
+          Thor
+          <input type="radio"
+            value="thor"
+            name="value"
+            checked={value === "thor"}
+            onChange={this.onChange} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Zues
+          <input type="radio"
+            value="zues"
+            name="value2"
+            checked={value2 === "zues"}
+            onChange={this.onChange} />
+        </label>
+        <label>
+          Hercules
+          <input type="radio"
+            value="hercules"
+            name="value2"
+            checked={value2 === "hercules"}
+            onChange={this.onChange} />
+        </label>
+      </form>
+    )
+  }
 }
 
 export default App;
